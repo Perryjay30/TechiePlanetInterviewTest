0f2eeeac11fca975ffd9ef4f5cb72289c4f9e7f4
perryjay30 <pelumijsh@gmail.com>
2025-10-26 11:34:37 -0700

    Question two - JUnit test

--- java-programs/question-two/src/test/java/com/techieplanet/DuplicateRemoverTest.java
+++ java-programs/question-two/src/test/java/com/techieplanet/DuplicateRemoverTest.java
@@ -38,4 +38,42 @@ public class DuplicateRemoverTest {
         assertEquals(0, out.length);
     }
 
+    @Test
+    void nullAndEmptyRows_shouldBeSafelyIgnored() {
+        int[][] in = new int[][]{
+                null,
+                {},
+                {5, 5, 5},
+                null,
+                {}
+        };
+        int[][] out = DuplicateRemover.dedupePerRow(in);
+        assertSame(in, out);
+
+        assertNull(out[0]);
+        assertEquals(0, out[1].length);
+        assertArrayEquals(new int[]{5, 0, 0}, out[2]);
+        assertNull(out[3]);
+        assertEquals(0, out[4].length);
+    }
+
+    @Test
+    void allSame_shouldZeroAllButFirst() {
+        int[][] in = new int[][]{
+                {4, 4, 4, 4},
+                {0, 0, 0}
+        };
+        int[][] out = DuplicateRemover.dedupePerRow(in);
+        assertArrayEquals(new int[]{4, 0, 0, 0}, out[0]);
+        assertArrayEquals(new int[]{0, 0, 0}, out[1]);
+    }
+
+    @Test
+    void interleavedDuplicates_shouldOnlyKeepFirstOccurrence() {
+        int[] row = {1, 2, 1, 3, 2, 4, 1, 5, 2};
+        int[] expected = {1, 2, 0, 3, 0, 4, 0, 5, 0};
+        int[][] in = new int[][]{row};
+        int[][] out = DuplicateRemover.dedupePerRow(in);
+        assertArrayEquals(expected, out[0]);
+    }
 }
